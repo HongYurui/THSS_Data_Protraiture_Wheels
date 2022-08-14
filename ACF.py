09c76ccd224772c3d70afc213a94c2f88d20704d
azishabibi <108248983+azishabibi@users.noreply.github.com>
2022-08-14 11:07:59 +0800

    Update ACF.py
    using time rather than str

--- ACF.py
+++ ACF.py
@@ -2,7 +2,6 @@ from FlokAlgorithmLocal import FlokAlgorithmLocal, FlokDataFrame
 import numpy as np
 import pandas as pd
 import time
-from datetime import datetime
 
 class acf(FlokAlgorithmLocal):
     def run(self, inputDataSets, params,time_=0):
@@ -12,23 +11,19 @@ class acf(FlokAlgorithmLocal):
         if timeseries:
             timeseries_list = timeseries.split(',')
             output_data = input_data[timeseries_list]
-            
-            for i in range(len(output_data)):
-                if (datetime.strptime(output_data.Time[i], '%Y-%m-%d %H:%M:%S') 
-                <= datetime.strptime(time_, '%Y-%m-%d %H:%M:%S')):
-                    count+=1
+            time0=output_data['Time'][0]
+            time0_ = time.mktime(time.strptime(time0, '%Y-%m-%d %H:%M:%S'))
+            count = int(time.mktime(time.strptime(
+                time_, '%Y-%m-%d %H:%M:%S'))-time0_)
             output_data.fillna(0, inplace=True)
             a = output_data[timeseries_list[1]][0:count]
             c = np.correlate(a, a, mode='full') / \
                 output_data[timeseries_list[1]].values[count-1]
             Time = []
-            for i in range(1, 2*count):
-                if i < 10:
-                    q = time_[0:-1]+str(i)
-                    Time.append(q)
-                if i >= 10:
-                    q = time_[0:-2]+str(i)
-                    Time.append(q)
+            for i in range(0, 2*count-1):
+                q = time.strftime('%Y-%m-%d %H:%M:%S',
+                                  time.localtime(time0_+i))
+                Time.append(q)
             j = 'acf({f})'.format(f=timeseries_list[1])
             data = {'Time': Time, j: c}
             output_data = pd.DataFrame(data)
@@ -37,9 +32,7 @@ class acf(FlokAlgorithmLocal):
         result = FlokDataFrame()
         result.addDF(output_data)
         return result
-
-       
-        
+      
 
 if __name__ == "__main__":
     algorithm = acf()
@@ -74,7 +67,7 @@ if __name__ == "__main__":
         "output": ["./test_out_2.csv"],
         "outputFormat": ["csv"],
         "outputLocation": ["local_fs"],
-        "parameters": {"timeseries": "Time,root.test.d2.s2"}
+        "parameters": {"timeseries": "Time,root.test.d1.s1"}
     }
 
     params = all_info_2["parameters"]
