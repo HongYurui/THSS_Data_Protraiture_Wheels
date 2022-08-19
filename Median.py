b025af155aab5c931cdcddbb3d10cc5fbb46b75c
HongYurui <hongyr20@mails.tsinghua.edu.cn>
2022-08-19 14:25:43 +0800

    rescue

--- Median.py
+++ Median.py
@@ -1,18 +1,15 @@
-from datetime import datetime
 import pandas as pd
 from FlokAlgorithmLocal import FlokAlgorithmLocal, FlokDataFrame
 
 class Median(FlokAlgorithmLocal):
     def run(self, inputDataSets, params):
         input_data = inputDataSets.get(0)
-        output_data = pd.DataFrame([[0] * input_data.shape[1]], index=range(1), columns=input_data.columns)
-        time_data = pd.Series([datetime.strptime(data, "%Y-%m-%d %H:%M:%S") for data in input_data.iloc[:, 0].values])
+        output_data = pd.DataFrame([[0, 0]], index=range(1), columns=input_data.columns)
         
         # calculation via pd.DataFrame.median()
-        for column in range(1, input_data.shape[1]):
-            output_data.iloc[0, column] = input_data.iloc[:, column].astype(float).median()
-        
-        output_data.iloc[0, 0] = time_data[0].strftime("%Y-%m-%d 00:00:00")
+        output_data.iloc[0, 0] = pd.to_datetime(input_data.iloc[0, 0], format="%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d 00:00:00.000")
+        output_data.iloc[0, 1] = input_data.iloc[:, 1].astype(float).median()
+
         result = FlokDataFrame()
         result.addDF(output_data)
         return result
