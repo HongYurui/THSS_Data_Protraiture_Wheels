606d3178a85da48a5dbd7ceb68eec9b4d468f1b2
HongYurui <hongyr20@mails.tsinghua.edu.cn>
2022-08-22 21:55:36 +0800

    modified sql.py

--- sql.py
+++ sql.py
@@ -2,11 +2,22 @@ import re
 from pandas import merge
 from pandasql import sqldf
 from FlokAlgorithmLocal import FlokDataFrame
+from Acf import Acf
+from Distinct import Distinct
+from Histogram import Histogram
 from Integral import Integral
+from Integralavg import Integralavg
 from Mad import Mad
 from Median import Median
+from Mode import Mode
+from Percentile import Percentile
+from Period import Period
+from Qlb import Qlb
 from Resample import Resample
 from Sample import Sample
+from Segment import Segment
+from SelectTimeseries import SelectTimeseries
+from 
 
 inputTypes = ['csv']
 inputLocation = ['local_fs']
@@ -15,14 +26,15 @@ outputTypes = ['csv']
 
 input_params_pairs: dict = {}
 
-while True:
-    command = input(">>> ")
+if True:
+    # command = input(">>> ")
     ## constant commands for debugging
     # command = "select Time, \"resample(s2, 'every'='1.0s', 'interp'='BFill', 'aggr'='Min', 'start'='2022-01-01 00:00:02', 'end'='2022-01-01 00:00:08')\" from root_test_d1 where Time <= '2022-01-01 00:00:12';"
     # command = "select Time, \"sample(s3, 'method'='isometric', 'k'='5')\" from root_test_d1 where Time <= '2022-01-01 00:00:12';"
-    # command = "select Time, \"median(s1)\" from root_test_d1 where Time <= '2022-01-01 00:00:12';"
+    command = "select Time, \"median(s1)\" from root_test_d1 where Time <= '2022-01-01 00:00:12';"
     if command in ["exit", "quit", "q"]:
-        break
+        # break
+        pass
     elif command in ["help", "h"]:
         print("""
         exit, quit, q: exit
@@ -32,7 +44,8 @@ while True:
     else:
         try:
             # preprocess the command by extracting patterns
-            pattern = re.findall(r"select\s+\w+,\s+\"(\w+)\((\w+),\s+(.*)\)\"\s+from\s+(.*?)[\s;]+.*", command)[0]
+            pattern = re.findall(r"select\s+\w+(,\s+\"(\w+)\((\w+),\s+(.*))?\)\"\s+from\s+(.*?)[\s;]+.*", command)[0]
+            print(pattern)
             funcName = pattern[0].capitalize()
             params = dict(re.findall(r"\'(\w+)\'\s*=\s*\'([\w\s\-\.:]+)\'", pattern[2]))
             if funcName == 'Sample' and params.get('method') == 'reservoir':
