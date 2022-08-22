3689e42ada6ca80501300c0741af838a832afb82
HongYurui <hongyr20@mails.tsinghua.edu.cn>
2022-08-23 00:53:46 +0800

    modified Acf so that it satisfied SQL queries

--- Acf.py
+++ Acf.py
@@ -34,6 +34,7 @@ class Acf(FlokAlgorithmLocal):
             #q = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time0_+i/1000.0))
             q=datetime.fromtimestamp((i+1)/1000.0)
             Time.append(q)
+        Time = pd.Series([t.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] for t in Time])
         j = 'acf({f})'.format(f=column)
         data = {'Time': Time, j: c}
         output_data = pd.DataFrame(data)
@@ -47,7 +48,7 @@ if __name__ == "__main__":
     algorithm = Acf()
 
     all_info_1 = {
-        "input": ["./test_in.csv"],
+        "input": ["root_test_d1"],
         "inputFormat": ["csv"],
         "inputLocation": ["local_fs"],
         "output": ["./test_out_1.csv"],
@@ -68,18 +69,18 @@ if __name__ == "__main__":
                              inputLocation, outputPaths, outputTypes)
     from SelectTimeseries import SelectTimeseries
     dataSet = SelectTimeseries().run(
-        dataSet, {"timeseries": "Time,root.test.d2.s2"})
+        dataSet, {"timeseries": "Time,s2"})
     result = algorithm.run(dataSet, params)
     algorithm.write(outputPaths, result, outputTypes, outputLocation)
     '''
     all_info_2 = {
-        "input": ["./test_in.csv"],
+        "input": ["root_test_d1"],
         "inputFormat": ["csv"],
         "inputLocation": ["local_fs"],
         "output": ["./test_out_2.csv"],
         "outputFormat": ["csv"],
         "outputLocation": ["local_fs"],
-        "parameters": {"timeseries": "Time,root.test.d2.s2"}
+        "parameters": {"timeseries": "Time,s2"}
     }
 
     params = all_info_2["parameters"]
