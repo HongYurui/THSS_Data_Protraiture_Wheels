6982f0fef59b48038c89e3f9f05fe48bb634d81c
NLno <105346235+NLno@users.noreply.github.com>
2022-08-22 09:33:41 +0800

    Update MvAvg.py

--- MvAvg.py
+++ MvAvg.py
@@ -2,10 +2,12 @@ from datetime import datetime
 import pandas as pd
 from FlokAlgorithmLocal import FlokAlgorithmLocal, FlokDataFrame
 
-class MVAVG(FlokAlgorithmLocal):
-    def run(self, inputDataSets, params, window=10):
+class Mvavg(FlokAlgorithmLocal):
+    def run(self, inputDataSets, params):
         input_data = inputDataSets.get(0)
         output_data = input_data.copy()
+
+        window = params.get("window", 10)
         for column in range(1, input_data.shape[1]):
             output_data.iloc[:, column] = input_data.iloc[:, column].rolling(window).mean()
         output_data = output_data.dropna()
@@ -15,7 +17,7 @@ class MVAVG(FlokAlgorithmLocal):
         return result
         
 if __name__ == "__main__":
-    algorithm = MVAVG()
+    algorithm = Mvavg()
 
     all_info_1 = {
         "input": ["./test_in.csv"],
@@ -24,7 +26,7 @@ if __name__ == "__main__":
         "output": ["./test_out_1.csv"],
         "outputFormat": ["csv"],
         "outputLocation": ["local_fs"],
-        "parameters": {"unit": "1S"}
+        "parameters": {"window": 10}
     }
 
     params = all_info_1["parameters"]
@@ -34,28 +36,6 @@ if __name__ == "__main__":
     outputPaths = all_info_1["output"]
     outputTypes = all_info_1["outputFormat"]
     outputLocation = all_info_1["outputLocation"]
-
-    dataSet = algorithm.read(inputPaths, inputTypes, inputLocation, outputPaths, outputTypes)
-    result = algorithm.run(dataSet, params)
-    algorithm.write(outputPaths, result, outputTypes, outputLocation)
-
-    all_info_2 = {
-        "input": ["./test_in.csv"],
-        "inputFormat": ["csv"],
-        "inputLocation": ["local_fs"],
-        "output": ["./test_out_2.csv"],
-        "outputFormat": ["csv"],
-        "outputLocation": ["local_fs"],
-        "parameters": {"timeseries": "Time,root.test.d2.s2"}
-    }
-
-    params = all_info_2["parameters"]
-    inputPaths = all_info_2["input"]
-    inputTypes = all_info_2["inputFormat"]
-    inputLocation = all_info_2["inputLocation"]
-    outputPaths = all_info_2["output"]
-    outputTypes = all_info_2["outputFormat"]
-    outputLocation = all_info_2["outputLocation"]
 
     dataSet = algorithm.read(inputPaths, inputTypes, inputLocation, outputPaths, outputTypes)
     result = algorithm.run(dataSet, params)
