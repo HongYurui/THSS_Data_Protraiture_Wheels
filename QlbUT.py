9a6c32e7cefa77c7e478854402322db51cc1851f
HongYurui <hongyr20@mails.tsinghua.edu.cn>
2022-08-23 22:03:12 +0800

    modified root_test_d1 and unittests

--- QlbUT.py
+++ QlbUT.py
@@ -12,19 +12,23 @@ class QlbUT(unittest.TestCase):
         input_location = ["local_fs"]
         output_paths = ["root_test_d1_out.csv"]
         output_types = ["csv"]
-        self.orif_dataset = FlokAlgorithmLocal().read(input_paths, input_types, input_location, output_paths, output_types)
+        self.orig_dataset = FlokAlgorithmLocal().read(input_paths, input_types, input_location, output_paths, output_types)
         self.algorithm = Qlb()
 
     def test_qlb_1(self):
-        self.timeseries = {"timeseries": "Time,s2"}
+        self.timeseries = {"timeseries": "Time,s13"}
+        self.serieslength = 20
         self.params = {}
 
     def test_qlb_2(self):
-        self.timeseries = {"timeseries": "Time,s2"}
+        self.timeseries = {"timeseries": "Time,s13"}
+        self.serieslength = 20
         self.params = {"lag": "5"}
 
     def tearDown(self):
-        dataset = SelectTimeseries().run(self.orif_dataset, self.timeseries)
+        dataset = FlokDataFrame()
+        dataset.addDF(SelectTimeseries().run(self.orig_dataset, self.timeseries).get(0).iloc[:self.serieslength])
+        Qlb().run(dataset, self.params)
         result = self.algorithm.run(dataset, self.params)
         print(result.get(0))
 
