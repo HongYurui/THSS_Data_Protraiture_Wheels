16905b8082a6dfe477c95013f0d65fd06d3639e9
NLno <105346235+NLno@users.noreply.github.com>
2022-08-24 00:17:34 +0800

    Update Mode.py

--- Mode.py
+++ Mode.py
@@ -1,18 +1,25 @@
 from datetime import datetime
+from math import nan
 import pandas as pd
 from FlokAlgorithmLocal import FlokAlgorithmLocal, FlokDataFrame
 
 class Mode(FlokAlgorithmLocal):
     def run(self, inputDataSets, params):
         input_data = inputDataSets.get(0)
+        input_data = input_data.dropna()
         # header format
         value_header = 'mode(' + input_data.columns[1]
         value_header += ')'
         output_data = pd.DataFrame([[0, 0]], index=range(1), columns=['Time', value_header])
+        input_type = type(input_data.iloc[1, 1])
         
         # calculation via pd.DataFrame.mode()
-        output_data.iloc[0, 1] = input_data.iloc[:, 1].astype(float).mode()[0]
-        output_data.iloc[0, 0] = datetime.strptime(input_data.iloc[0, 0], "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d 00:00:00")
+        nannum = input_data.iloc[:, 1].isna().sum()
+        if nannum > (input_data.iloc[:, 1] == input_data.iloc[:, 1].astype(input_type).mode()[0]).sum():
+                output_data.iloc[0, 1] = nan
+        else:
+            output_data.iloc[0, 1] = input_data.iloc[:, 1].mode()[0]
+        output_data.iloc[0, 0] = datetime.strptime('1970-01-01 08:00:00', "%Y-%m-%d %H:%M:%S").strftime("%Y-%m-%d 08:00:00")
         
         result = FlokDataFrame()
         result.addDF(output_data)
