a27d1c3a091d4a90412b9eeb58d37590cc7d0939
azishabibi <azis2238437609@gmail.com>
2022-08-24 12:51:40 +0800

    fix some bug

--- Spline.py
+++ Spline.py
@@ -4,9 +4,67 @@ import numpy as np
 from scipy.interpolate import CubicSpline
 import pandas as pd
 from datetime import datetime
+from typing import Tuple, List
+import bisect
 
 
 class Spline(FlokAlgorithmLocal):
+    def compute_changes(x: List[float]) -> List[float]:
+        return [x[i+1] - x[i] for i in range(len(x) - 1)]
+
+    def create_tridiagonalmatrix(n: int, h: List[float]) -> Tuple[List[float], List[float], List[float]]:
+        A = [h[i] / (h[i] + h[i + 1]) for i in range(n - 2)] + [0]
+        B = [2] * n
+        C = [0] + [h[i + 1] / (h[i] + h[i + 1]) for i in range(n - 2)]
+        return A, B, C
+
+    def create_target(n: int, h: List[float], y: List[float]):
+        return [0] + [6 * ((y[i + 1] - y[i]) / h[i] - (y[i] - y[i - 1]) / h[i - 1]) / (h[i] + h[i-1]) for i in range(1, n - 1)] + [0]
+
+    def solve_tridiagonalsystem(A: List[float], B: List[float], C: List[float], D: List[float]):
+        c_p = C + [0]
+        d_p = [0] * len(B)
+        X = [0] * len(B)
+
+        c_p[0] = C[0] / B[0]
+        d_p[0] = D[0] / B[0]
+        for i in range(1, len(B)):
+            c_p[i] = c_p[i] / (B[i] - c_p[i - 1] * A[i - 1])
+            d_p[i] = (D[i] - d_p[i - 1] * A[i - 1]) / \
+                (B[i] - c_p[i - 1] * A[i - 1])
+
+        X[-1] = d_p[-1]
+        for i in range(len(B) - 2, -1, -1):
+            X[i] = d_p[i] - c_p[i] * X[i + 1]
+
+        return X
+
+    def compute_spline(x: List[float], y: List[float]):
+        n = len(x)
+        if n < 3:
+            raise ValueError('Too short an array')
+        if n != len(y):
+            raise ValueError('Array lengths are different')
+
+        h = Spline.compute_changes(x)
+        if any(v < 0 for v in h):
+            raise ValueError('X must be strictly increasing')
+
+        A, B, C = Spline.create_tridiagonalmatrix(n, h)
+        D = Spline.create_target(n, h, y)
+
+        M = Spline.solve_tridiagonalsystem(A, B, C, D)
+        coefficients = [[(M[i+1]-M[i])*h[i]*h[i]/6, M[i]*h[i]*h[i]/2, (y[i+1] -
+                                                                       y[i] - (M[i+1]+2*M[i])*h[i]*h[i]/6), y[i]] for i in range(n-1)]
+
+        def spline(val):
+            idx = min(bisect.bisect(x, val)-1, n-2)
+            z = (val - x[idx]) / h[idx]
+            C = coefficients[idx]
+            return (((C[0] * z) + C[1]) * z + C[2]) * z + C[3]
+
+        return spline
+
     def run(self, inputDataSets, params):
         input_data = inputDataSets.get(0)
         output_data = input_data
@@ -25,14 +83,17 @@ class Spline(FlokAlgorithmLocal):
             value = output_data[column]
             # （t，c，k）包含节点向量、B样条曲线系数和样条曲线阶数的元组。
             #tck = interpolate.splrep(Time, value, k=3)
-            c = CubicSpline(Time, value, bc_type=((1, 4.9), (1, 9.9)))
+            #c = CubicSpline(Time, value, bc_type=((1, 4.9), (1, 9.9)))
+            spline = Spline.compute_spline(Time, value)
             x = (np.linspace(min(Time), max(Time), points)).tolist()
-            y=list(c(x))
+            y = [spline(t) for t in x]
+            # y=list(c(x))
             #y = (interpolate.splev(x, tck, der=0)).tolist()
             for i in range(0, len(x)):
                 x[i] = datetime.fromtimestamp(x[i])
-                #print(type(x[i]))
-            Time = pd.Series([t.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] for t in x])
+                # print(type(x[i]))
+            Time = pd.Series(
+                [t.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3] for t in x])
             j = 'spline({},\'points\'=\'{}\')'.format(column, points)
             data = {'Time': x, j: y}
             output_data = pd.DataFrame(data)
